e1520eb598c42a0250f2d92fbaed36155d7cc3cf
Jonathan Guerra <jguerrap1@ucentral.edu.co>
2021-10-09 22:49:31 -0500

    first commit

---
+++ src/components/FilterByName.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="search">
+    <input
+      type="text"
+      placeholder="Search by name"
+      v-model="name"
+      
+      @keyup="filter()"
+    >
+    <!--
+    Keyup funciona para que cuando se presione una tecla,
+    se efecute la funcion
+    -->
+
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+import { useStore } from 'vuex'
+
+export default {
+  setup() {
+    const name = ref('')
+    const store = useStore()
+    const filter = () => {
+      store.dispatch('filterByName', name.value)
+    }
+    return {
+      name,
+      filter
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.search {
+  width: 400px;
+  margin: 3rem auto 0;
+  input {
+    height: 53px;
+    width: 400px;
+    border: none;
+    border-radius: 10px;
+    padding: 0 0.5rem;
+  }
+}
+</style>
